55dac43a1b34a6f7e966bec6ee63f4a60e34b7ad
nanda242825 <102916245+nanda242825@users.noreply.github.com>
2022-07-18 06:22:15 -0700

    Add files via upload

---
+++ Main2Activity.java
@@ -0,0 +1,65 @@
+package com.example.formdata;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @BindView(R.id.input_nama)
+    EditText inputNama;
+    @BindView(R.id.input_nik)
+    EditText inputNik;
+    @BindView(R.id.input_umur)
+    EditText inputUmur;
+    @BindView(R.id.input_alamat)
+    EditText inputAlamat;
+    @BindView(R.id.input_institusi)
+    EditText inputInstitusi;
+
+    @BindView(R.id.simpan_button)
+    Button simpan_button;
+
+    @BindView(R.id.hasil_nama)
+    TextView hasilNama;
+    @BindView(R.id.hasil_nik)
+    TextView hasilNik;
+    @BindView(R.id.hasil_umur)
+    TextView hasilUmur;
+    @BindView(R.id.hasil_alamat)
+    TextView hasilAlamat;
+    @BindView(R.id.hasil_institusi)
+    TextView hasilInstitusi;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.simpan_button)
+    public void btnClicked() {
+        String nama = inputNama.getText().toString();
+        hasilNama.setText(nama);
+        String nik = inputNik.getText().toString();
+        hasilNik.setText(nik);
+        String umur = inputUmur.getText().toString();
+        hasilUmur.setText(umur);
+        String alamat = inputAlamat.getText().toString();
+        hasilAlamat.setText(alamat);
+        String institusi = inputInstitusi.getText().toString();
+        hasilInstitusi.setText(institusi);
+    }
+
+    public void submit(View view) {
+    }
+}
